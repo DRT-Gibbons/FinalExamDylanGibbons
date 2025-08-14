4752c71a7df374ddde851f81277f18a29ff2fe96
DRT-Gibbons <drtgibb@gmail.com>
2025-08-14 14:43:30 -0400

    Question 4.2 complete

--- src/main/java/com/example/finalexamjavadylangibbons/Customer.java
+++ src/main/java/com/example/finalexamjavadylangibbons/Customer.java
@@ -8,6 +8,22 @@ public class Customer {
     public String lastName;
     public String phoneNumber;
     public Integer[] purchases;
+    public double salePrice;
+    public double[] regularPrices;
+    public double[] salePrices;
+
+    public double getTotalSaved() {
+        double totalSaved = 0.0;
+
+        // length set this way to avoid index issues
+        int length = Math.min(regularPrices.length, salePrices.length);
+        //simple for loop
+        for (int i = 0; i < length; i++) {
+            //getting difference between for each set and adding to total saved
+            totalSaved += (regularPrices[i] - salePrices[i]);
+        }
+        return totalSaved;
+    }
 
     // Method to return total purchases as double using streams
     public double getTotalPurchases() {
