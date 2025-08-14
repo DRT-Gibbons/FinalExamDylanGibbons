44ad57f5821f8d19e27607d73467834774681e4e
DRT-Gibbons <drtgibb@gmail.com>
2025-08-14 13:57:50 -0400

    question 2 complete

--- src/main/java/com/example/finalexamjavadylangibbons/Product.java
+++ src/main/java/com/example/finalexamjavadylangibbons/Product.java
@@ -1,14 +1,20 @@
 package com.example.finalexamjavadylangibbons;
 
 public class Product {
-  public int userId;
   public int id;
-  public String title;
-  public String body;
+  public String sku;
+  public String name;
+  public double salePrice;
+  public double regularPrice;
 
   @Override
   public String toString() {
-    // Format the post data nicely for display in UI or console
-    return "Post #" + id + " (User " + userId + "): " + title + "\n" + body;
+    return name + "-$" + salePrice;
   }
 }
+
+
+//This class should track the sku, name, sale price, regular price and a String to hold the url information for
+//the image. This class should also have a toString() method.
+//The toString() method should format the response as “[name]-$[sale price]”.
+//For example “T-shirt with Logo-$17.00”.
